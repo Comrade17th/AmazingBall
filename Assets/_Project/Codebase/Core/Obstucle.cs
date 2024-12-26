c2478c98d9bb3ff42b4d3bc74af8a2c783218f3a
Ravshan <ravshannedonat@gmail.com>
2024-12-26 20:46:38 +0300

    move toward XZ works, goes throw the ground(((

---
+++ Assets/_Project/Codebase/Core/Obstucle.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace _Project.Codebase.Core
+{
+    public class Obstucle : MonoBehaviour
+    {
+        
+    }
+}
--- Assets/_Project/Codebase/Core/PhysicsBody.cs
+++ Assets/_Project/Codebase/Core/PhysicsBody.cs
@@ -1,22 +1,30 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace _Project.Codebase.Core
 {
+    [RequireComponent(typeof(Collider))]
     public class PhysicsBody : MonoBehaviour
     {
-        [SerializeField] private float _friction = 0.5f;
+        private readonly Vector3 Gravity = new Vector3(0f, -9.81f, 0f);
         
-        [SerializeField] private float _velocity = 3;
-        private Vector3 _direction = Vector3.forward;
+        [SerializeField] private float _friction = 0.1f; // sepparate air friction and ground friction
+        [SerializeField] private float _dampingImpactPercent = 0.1f;
+        
+        [SerializeField] private Vector3 _velocity = new Vector3(0, 0, 0);
+        [SerializeField] private Vector3 _acceleration = new Vector3(0, 0, 0);
+        
 
         private void FixedUpdate()
         {
-            if (_velocity <= 0.0f)
-                _velocity = 0;
-            else
-                _velocity -= _friction * Time.deltaTime;
+            _velocity += Gravity * Time.fixedDeltaTime;
             
-            transform.Translate(_direction * (_velocity * Time.fixedDeltaTime));
+            _velocity = new Vector3(
+                Mathf.MoveTowards(_velocity.x, 0, _friction),
+                _velocity.y,
+                Mathf.MoveTowards(_velocity.z, 0, _friction));
+            
+            transform.Translate(_velocity * Time.fixedDeltaTime);
         }
 
         public void AddVelocity(float velocity, Vector3 direction)
@@ -24,9 +32,29 @@ namespace _Project.Codebase.Core
             
         }
 
-        private void AddFriction()
+        private void OnCollisionEnter(Collision collision)
         {
+            // Debug.Log("Points colliding: " + collision.contacts.Length);
+            // Debug.Log("Normal of the first point: " + collision.contacts[0].normal);
             
+            var contatcs = new List<ContactPoint>();
+            collision.GetContacts(contatcs);
+            
+            ContactPoint contact = contatcs[0];
+            Vector3 reflectedDirection = Vector3.Reflect(_velocity, contact.normal);
+            
+            //bug.Log($"{} {reflectedDirection}");
+            _velocity = reflectedDirection;
+            _velocity -= _velocity * _dampingImpactPercent;
+            
+            
+            // foreach (var item in collision.contacts)
+            // {
+            //     Debug.DrawRay(item.point, _direction * -100, Color.blue, 10f);
+            //     Debug.DrawRay(item.point, item.normal * 100, Color.cyan, 10f);
+            //     Debug.DrawRay(item.point,  Vector3.Reflect(_direction, item.normal) * 100, Color.magenta, 10f);
+            //     Debug.Log($"origin normal {item.point} normalized: {item.point.normalized}");
+            //}
         }
     }
 }
