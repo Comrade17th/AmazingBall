fb40d602b035a552acae0755cace6a26ce5ac032
Ravshan <ravshannedonat@gmail.com>
2024-12-26 15:14:34 +0300

    Simple forward move

--- Assets/_Project/Codebase/Core/PhysicsBody.cs
+++ Assets/_Project/Codebase/Core/PhysicsBody.cs
@@ -1,4 +1,32 @@
-public class PhysicsBody
+using UnityEngine;
+
+namespace _Project.Codebase.Core
 {
-    
+    public class PhysicsBody : MonoBehaviour
+    {
+        [SerializeField] private float _friction = 0.5f;
+        
+        [SerializeField] private float _velocity = 3;
+        private Vector3 _direction = Vector3.forward;
+
+        private void FixedUpdate()
+        {
+            if (_velocity <= 0.0f)
+                _velocity = 0;
+            else
+                _velocity -= _friction * Time.deltaTime;
+            
+            transform.Translate(_direction * (_velocity * Time.fixedDeltaTime));
+        }
+
+        public void AddVelocity(float velocity, Vector3 direction)
+        {
+            
+        }
+
+        private void AddFriction()
+        {
+            
+        }
+    }
 }
