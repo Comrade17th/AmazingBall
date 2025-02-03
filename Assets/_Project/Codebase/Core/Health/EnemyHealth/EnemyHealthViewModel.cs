df6f367bf15bf5b8acb07e74f7dea59bd4eb5fe5
nasusfen <ravshannedonat@gmail.com>
2025-02-03 13:24:48 +0300

    still not fixed

--- Assets/_Project/Codebase/Core/Health/EnemyHealth/EnemyHealthViewModel.cs
+++ Assets/_Project/Codebase/Core/Health/EnemyHealth/EnemyHealthViewModel.cs
@@ -1,9 +1,11 @@
 using _Project.Codebase.Core.Health.GeneralHealth;
+using Zenject;
 
 namespace _Project.Codebase.Core.Health.EnemyHealth
 {
 	public class EnemyHealthViewModel : HealthViewModel, IEnemyHealthViewModel
 	{
+		[Inject]
 		public EnemyHealthViewModel(
 			IEnemyHealthModel model,
 			IEnemyHealthView view,
